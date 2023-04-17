5ef7581c1c6a980a6bdce929bb8159aa7acffeea
Ирина Капкова <kapiral@rambler.ru>
2023-04-17 23:19:00 +0400

    1 commit

---
+++ src/test/java/me/ikapkova/tests/TestsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ikapkova.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
